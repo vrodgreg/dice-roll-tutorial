4532f6b050260db9e4c38c7b44270b4cec574f4e
Greg King <vrodgreg@gmail.com>
2021-02-11 23:38:31 -0500

    Dice from tutorial.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [firstDieResult, setFirstDieResult] = useState(1);
+  const [secondDieResult, setSecondDieResult] = useState(6);
+
+  const firstDieImage = require(`./assets/${firstDieResult}.png`);
+  const secondDieImage = require(`./assets/${secondDieResult}.png`);
+
+  function rollDice() {
+    setFirstDieResult(Math.floor(Math.random() * 6) + 1);
+    setSecondDieResult(Math.floor(Math.random() * 6) + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: 'flex', margin: 20 }}>
+          <img src={firstDieImage} className="die" alt="Die one" />
+          <img src={secondDieImage} className="die" alt="Die two" />
+        </div>
+        <span>{firstDieResult + secondDieResult}</span>
+        <button className="button" onClick={rollDice}>Roll</button>
       </header>
     </div>
   );
